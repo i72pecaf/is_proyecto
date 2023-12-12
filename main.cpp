e86d97efaf0acf6ffcd8e886ed0c66d6b0054a5d
i72pecaf <fjperezcast@gmail.com>
2023-12-12 20:16:14 +0100

    Main, leer datos de usuario
    Usando ficheros se ha conseguido comprobar un usuario por terminal y verificar que existe y obtener su codigo de rol

--- main.cpp
+++ main.cpp
@@ -1,16 +1,26 @@
 #include <iostream>
 #include <fstream>
+#include <sstream> // Para facilitar el manejo de cadenas
 
 using namespace std;
 
 int main(){
+    // -- Bloque de declaración de variables usadas
     int opt;
-    //int rol; // 0: visitante, 1: director academico, 2: organizador, 3: usuario registrado
-    string correo;
+    int rol = 0 ; // 0: visitante, 1: director academico, 2: organizador, 3: usuario registrado
+    
+    string email;
+    string linea, subCadena;
+    string auxRol;
+    
+    string ficheroDatos = "datosUsuariosUCO.txt"; // Fichero con los datos de los usuarios, su rol y correo
 
     ifstream f;
-    string ficheroDatos = "datosUsuariosUCO.txt"; // Fichero con los datos de los usuarios, su rol y correo
+
+    bool flagCorreo = false; // Para saber si se ha encontrado el correo o no.
+
     
+    // -- Bloque de código
     cout << "\n---------------------------------------------" << endl;
     cout << "Bienvenido al gestor de actividades de la UCO" << endl;
     cout << "---------------------------------------------" << endl;
@@ -24,24 +34,40 @@ int main(){
     switch(opt){
         case 1: // Modo logueado
             cout << "\nIntroduce tu correo de la UCO: "; 
-            cin >> correo;
+            cin >> email;
+           
             // Se procede a comprobar si existe el usuario con ese correo
-            cout << "\nEl correo introducido ha sido" << correo << endl;
-
-            f.open(ficheroDatos);
-
+            f.open(ficheroDatos); // Abrimos el fichero
             if(!f.is_open()){ // Manejo de errores, caso donde no se abra bien el fichero
                 cout << "Error al abrir el fichero" << endl;
                 break; // Salimos del switch en caso de error
             }
 
-            string var;
-            while (getline(f, var, ' '))
-            {
-                cout << "Un valor: " << lectura << endl;
+            while (getline(f, linea)) { // Vamos linea por linea buscando correos
+                istringstream stream(linea); // Objeto para leer las palabras de la cadena (linea del fichero)
+                
+                while (stream >> subCadena) { // Vamos por cada una de las palabras de la linea del fichero
+                    if(subCadena == email){ // Si encontramos el correo actualizamos la flag.
+                        flagCorreo = true;
+                    }
+
+                    if(flagCorreo) { // En caso de haber leido el correo, la siguiente palabra es su rol.
+                        auxRol = subCadena; // Pasamos la cadena a un numero int, el cual sera el rol.
+                    }
+                }
+                if (flagCorreo) { // Si encontramos el correo, dejamos de buscar
+                    break; // Nos salimos del while de leer el fichero
+                } // En caso contrario, seguimos leyendo
             }
 
             f.close();
+
+            if(!flagCorreo){
+                cout << "Error: No se ha encontrado un usuario con ese correo" << endl;
+                break; // Salimos del switch en caso de error
+            }
+        
+            cout << "El rol de tu correo es: " << rol << endl;
 
         break;
         
