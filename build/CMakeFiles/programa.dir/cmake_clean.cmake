8976443c3fe8b18f822ac491915ed9169128efc0
i72pecaf <fjperezcast@gmail.com>
2023-12-18 18:50:38 +0100

    Modificacion de la estructura del proyecto y compilacion
    Se ha decidido usar la herramienta cmake para la construccion y compilado del programa.
    Se ha creado la carpeta src donde se guardara el codigo del programa, tanto cpp como hpp. En la carpeta build se guardaran los ficheros relacionados con la compilacion, junto con el CMakeLists.txt y el propio ejecutable del programa.

---
+++ build/CMakeFiles/programa.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/programa.dir/src/director.cpp.o"
+  "CMakeFiles/programa.dir/src/director.cpp.o.d"
+  "CMakeFiles/programa.dir/src/mail.cpp.o"
+  "CMakeFiles/programa.dir/src/mail.cpp.o.d"
+  "CMakeFiles/programa.dir/src/main.cpp.o"
+  "CMakeFiles/programa.dir/src/main.cpp.o.d"
+  "CMakeFiles/programa.dir/src/organizador.cpp.o"
+  "CMakeFiles/programa.dir/src/organizador.cpp.o.d"
+  "CMakeFiles/programa.dir/src/panelActividades.cpp.o"
+  "CMakeFiles/programa.dir/src/panelActividades.cpp.o.d"
+  "CMakeFiles/programa.dir/src/usuarioRegistrado.cpp.o"
+  "CMakeFiles/programa.dir/src/usuarioRegistrado.cpp.o.d"
+  "CMakeFiles/programa.dir/src/usuarioVisitante.cpp.o"
+  "CMakeFiles/programa.dir/src/usuarioVisitante.cpp.o.d"
+  "programa"
+  "programa.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/programa.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
